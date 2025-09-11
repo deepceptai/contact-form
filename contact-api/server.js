53cd9448a301b8601c524b8e88c559cd0213e06c
Karthikramesh12 <karthiknee12@gmail.com>
2025-09-11 12:51:48 +0530

    added cors

--- contact-api/server.js
+++ contact-api/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import contactRoutes from './route/contactRoute.js'
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", contactRoutes);
 
